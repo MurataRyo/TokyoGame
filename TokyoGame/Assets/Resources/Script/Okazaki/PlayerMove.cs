74636103e9e2d79fc7e41c878f5a87dfa9a842c1
takumi okazaki <takumi9912@gmail.com>
2019-02-17 16:34:12 +0900

    不具合修正、コメント追加

--- TokyoGame/Assets/Resources/Script/Okazaki/PlayerMove.cs
+++ TokyoGame/Assets/Resources/Script/Okazaki/PlayerMove.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    float speed;
-    float jumpPower;
-    private const float WALK_SPEED = 3f;
-    private const float RUN_SPEED = 4f;
-    private const float JUMP_HEIGHT = 2.5f;
-    private const float GRAVITY_SIZE = 9.81f;
-    bool runFlag = false;
-    bool isGround = false;
-    bool lightFlag = false;
-    bool jumpFlag = false;
+    float speed;                                // 移動速度
+    float jumpPower;                            // ジャンプ力
+    private const float WALK_SPEED = 3f;        // 歩行速度
+    private const float RUN_SPEED = 4f;         // 走行速度
+    private const float JUMP_HEIGHT = 2.5f;     // ジャンプの頂点
+    private const float GRAVITY_SIZE = 9.81f;   // 重力の強さ
+    bool runFlag = false;                       // 走るかどうか
+    bool isGround = false;                      // 接地しているかどうか
+    bool isRightWall = false;                   // 右の壁に触れているかどうか
+    bool isLeftWall = false;                    // 左の壁に触れているかどうか
+    bool lightFlag = false;                     // 光に当たっているかどうか（現在未使用）
+    bool jumpFlag = false;                      // ジャンプするかどうか
     const float DISTANCE = 0.1f;
 
     new Rigidbody2D rigidbody;
@@ -21,7 +23,7 @@ public class PlayerMove : MonoBehaviour
 
     void Start()
     {
-        jumpPower = Mathf.Pow(JUMP_HEIGHT * 2 * GRAVITY_SIZE, 0.5f);
+        jumpPower = Mathf.Pow(JUMP_HEIGHT * 2 * GRAVITY_SIZE, 0.5f); // ジャンプ力の計算
         rigidbody = GetComponent<Rigidbody2D>();
         collider2D = GetComponent<BoxCollider2D>();
     }
@@ -30,7 +32,8 @@ public class PlayerMove : MonoBehaviour
     {
         Vector2 velocity = rigidbody.velocity;
 
-        if(Input.GetKey(KeyCode.D))
+        // 右に移動
+        if(Input.GetKey(KeyCode.D) && !isRightWall)
         {
             if(runFlag)
             {
@@ -41,7 +44,8 @@ public class PlayerMove : MonoBehaviour
                 speed = WALK_SPEED;
             }
         }
-        else if (Input.GetKey(KeyCode.A))
+        // 左に移動
+        else if (Input.GetKey(KeyCode.A) && !isLeftWall)
         {
             if (runFlag)
             {
@@ -66,6 +70,7 @@ public class PlayerMove : MonoBehaviour
             runFlag = false;
         }
 
+        // ジャンプする
         if (Input.GetKeyDown(KeyCode.Space) && isGround)
         {
             jumpFlag = true;
@@ -76,6 +81,7 @@ public class PlayerMove : MonoBehaviour
             jumpFlag = false;
         }
 
+        /*重力関係---------------------------------------------------*/
         if (isGround && !jumpFlag)
         {
             velocity.y = 0f;
@@ -89,18 +95,35 @@ public class PlayerMove : MonoBehaviour
             velocity.y -= GRAVITY_SIZE * Time.deltaTime;
         }
 
+        if(Input.GetKeyUp(KeyCode.Space) && velocity.y > 0f)
+        {
+            velocity /= 3f;
+        }
+        /*----------------------------------------------------------*/
+
         rigidbody.velocity = new Vector2(speed, velocity.y);
     }
 
+    // 接地・接触判定
     private void OnCollisionEnter2D(Collision2D collision)
     {
         isGround = false;
+        isRightWall = false;
+        isLeftWall = false;
 
-        for(int i = 0; i < collision.contacts.Length; i++)
+        for (int i = 0; i < collision.contacts.Length; i++)
         {
             if(collision.contacts[i].normal.y > 0.5f)
             {
                 isGround = true;
+            }
+            if(collision.contacts[i].normal.x > 0.5f)
+            {
+                isLeftWall = true;
+            }
+            if(collision.contacts[i].normal.x < -0.5f)
+            {
+                isRightWall = true;
             }
         }
     }
