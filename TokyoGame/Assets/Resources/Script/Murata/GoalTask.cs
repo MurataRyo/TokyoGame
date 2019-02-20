69a56509efcde03af2c34386f56c61e5737be124
murata <eru1008ryo@gmail.com>
2019-02-21 01:38:44 +0900

    ゴールコライダーの取得用追加 Tasksタグの追加

---
+++ TokyoGame/Assets/Resources/Script/Murata/GoalTask.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalTask : MonoBehaviour
+{
+    [HideInInspector]public List<BoxCollider2D> rayColliders;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rayColliders = new List<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //レイの追加
+    public void AddRayColider(BoxCollider2D col)
+    {
+        rayColliders.Add(col);
+    }
+
+    //レイの削除
+    public void RemoveRayColider(BoxCollider2D col)
+    {
+        rayColliders.Remove(col);
+    }
+}
---
+++ TokyoGame/Assets/Resources/Script/Murata/Utility.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utility : MonoBehaviour
+{
+    public static GameObject GetTask()
+    {
+        return GameObject.FindGameObjectWithTag("Tasks");
+    }
+}
