2a2ae488ac50f15415d1bc805515f2ca13c2346d
kozaki <s.takutaku317@gmail.com>
2019-02-25 11:42:02 +0900

    レイの修正

--- TokyoGame/Assets/Resources/Script/Suto/test.cs
+++ TokyoGame/Assets/Resources/Script/Suto/test.cs
@@ -12,15 +12,6 @@ public class test : MonoBehaviour
     GameObject taskObject;
     GoalTask goalTask;
 
-    #region
-    //[HideInInspector] LineRenderer line; //線
-    //[HideInInspector] float counter; // 伸びるカウント
-    //[HideInInspector] float dist; //距離
-    //[HideInInspector] GameObject startobj; //スタートオブジェクト
-    //[HideInInspector] GameObject goalobj; //目標オブジェクト
-    //[HideInInspector] float drawSpeed = 6f; //出る速さ
-    #endregion
-
     void Start()
     {
         //　コライダーを入れる子オブジェクト
@@ -35,54 +26,7 @@ public class test : MonoBehaviour
         taskObject =
             Utility.GetTask();
         goalTask = taskObject.GetComponent<GoalTask>();
-        #region
-        //foreach (Transform chiild in transform)
-        //{
-        //    if (chiild.gameObject.tag == "Launch")
-        //    {
-        //        startobj = chiild.gameObject;
-        //        break;
-        //    }
-        //}
-        //startobj = GameObject.FindGameObjectWithTag("Launch");
-        //goalobj = GameObject.FindGameObjectWithTag("Mirror");
-        //line = GetComponent<LineRenderer>();
-        //line.SetPosition(0, startobj.transform.position);
-
-        //// 距離
-        //dist = Vector3.Distance(startobj.transform.position, goalobj.transform.position);
-        #endregion
         originate = new List<Vec2Class>();
-    }
-
-    void Update()
-    {
-
-        //foreach(RaycastHit2D hits in Physics2D.RaycastAll(transform.position, transform.right))
-        //{
-        //    Debug.Log(hits.collider.gameObject.name);
-        //}
-
-        #region
-        ////カウントが距離よりも小さいとき
-        //if (counter < dist)
-        //{
-        //    //この速さで出る
-        //    counter += 0.5f / drawSpeed;
-
-        //    //どこまで伸びているのか補完
-        //    float x = Mathf.Lerp(0, dist, counter);
-
-        //    //目標とスタートの座標を入れる
-        //    Vector3 pointA = startobj.transform.position;
-        //    Vector3 pointB = goalobj.transform.position;
-
-        //    //今どこまで伸びているのか計算
-        //    Vector3 pointLine = x * Vector3.Normalize(pointB - pointA) + pointA;
-
-        //    line.SetPosition(1, pointLine);
-        //}
-        #endregion
     }
 
     //レイをシーンで見えるようにする
@@ -104,7 +48,7 @@ public class test : MonoBehaviour
         foreach (RaycastHit2D hit in Physics2D.RaycastAll(position + direction * 0.5f, direction))
         {
             BoxCol2D(hit.point);
-            if (IsRefrect(hit)) //鏡に当たった時
+            if (IsRefrect(hit)) //光が反射
             {
                 direction = Vector2.Reflect(direction, hit.normal);
                 position = hit.point;
@@ -112,8 +56,9 @@ public class test : MonoBehaviour
                 originate.Add(new Vec2Class(position));
                 Debug.Log("鏡に当たった");
                 DrawReflect(position, direction);
+                break;
             }
-            else if (NotRefrect(hit)) //当たらないものと鏡以外に当たった時
+            else if (NotRefrect(hit)) //光が終了
             {
                 direction = Vector2.Reflect(direction, hit.normal);
                 position = hit.point;
@@ -121,10 +66,9 @@ public class test : MonoBehaviour
                 Debug.Log("反射しません");
                 break;
             }
-            else //それ以外
+            else //光が貫通
             {
-                position += direction * maxStep;
-                originate.Add(new Vec2Class(position));
+                continue;
             }
 
 
