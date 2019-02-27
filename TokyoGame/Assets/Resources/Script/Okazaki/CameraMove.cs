9046838eb1328fe867e6452bc7c24d73750df796
takumi okazaki <takumi9912@gmail.com>
2019-02-28 01:52:15 +0900

    自機のモデルを変更、カメラを自機に追従、サブカメラ仮追加

---
+++ TokyoGame/Assets/Resources/Script/Okazaki/CameraMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    GameObject Player;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+    
+    void Update()
+    {
+        transform.position = new Vector3(0f, 3f, -8f) + Player.transform.position;
+    }
+}
