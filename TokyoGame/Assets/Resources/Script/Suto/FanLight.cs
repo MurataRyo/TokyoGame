d2f6209bc6b0137d7ef70dd541b975f77916c13d
kozaki <s.takutaku317@gmail.com>
2019-03-20 17:55:58 +0900

    扇のコライダー修正

--- TokyoGame/Assets/Resources/Script/Suto/FanLight.cs
+++ TokyoGame/Assets/Resources/Script/Suto/FanLight.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.U2D;
+using System.Linq;
 
 public class FanLight : MonoBehaviour
 {
@@ -13,39 +14,46 @@ public class FanLight : MonoBehaviour
     void FixedUpdate()
     {
         shapeController.spline.Clear();
-        shapeController.spline.InsertPointAt(0, new Vector3(0f, 0f, 0f));
-        for (int i = 0; i < vertexCount; i++)
+        Collider2D[] col2D = Physics2D.OverlapCircleAll(transform.position, 0.01f);
+        if (col2D.Where(col => !NotRefrect(col.tag)).ToArray().Length == 0)
         {
-            Vector3 endPoint = new Vector3(Mathf.Cos(angle / (vertexCount - 1) * i * Mathf.Deg2Rad) * range, Mathf.Sin(angle / (vertexCount - 1) * i * Mathf.Deg2Rad) * range, 0f);
-            RaycastHit2D[] hit = Physics2D.RaycastAll(transform.position, endPoint.normalized, range);
-            foreach (RaycastHit2D hit2D in hit)
+            shapeController.spline.InsertPointAt(0, new Vector3(0f, 0f, 0f));
+            for (int i = 0; i < vertexCount; i++)
             {
-               if(NotRefrect(hit2D))
+                Vector2 endPoint = new Vector2(Mathf.Cos(angle / (vertexCount - 1) * i * Mathf.Deg2Rad) * range, Mathf.Sin(angle / (vertexCount - 1) * i * Mathf.Deg2Rad) * range);
+                RaycastHit2D[] hit = Physics2D.RaycastAll(transform.position, endPoint.normalized, range);
+                foreach (RaycastHit2D hit2D in hit)
                 {
-                    continue;
+                    if (NotRefrect(hit2D.collider.tag))
+                    {
+                        continue;
+                    }
+                    endPoint = transform.InverseTransformPoint(hit2D.point);
+                    break;
                 }
-                Debug.Log(hit2D.collider.name);
-                endPoint = transform.InverseTransformPoint(hit2D.point);
-                break;
+                shapeController.spline.InsertPointAt(0, endPoint);
             }
-            shapeController.spline.InsertPointAt(0, endPoint);
+            shapeController.BakeCollider();
         }
-        shapeController.BakeCollider();
+        else
+        {
+            shapeController.polygonCollider.pathCount = 0;
+        }
     }
 
     //光が貫通するかどうか
-    bool NotRefrect(RaycastHit2D hit)
+    bool NotRefrect(string tag)
     {
-        if (hit.collider.tag == GetTag.Refrect)
+        if (tag == GetTag.Refrect)
             return false;
 
-        if (hit.collider.tag == GetTag.Mirror_Back)
+        if (tag == GetTag.Mirror_Back)
             return false;
 
-        if (hit.collider.tag == GetTag.Block)
+        if (tag == GetTag.Block)
             return false;
 
-        if (hit.collider.tag == GetTag.NotRefrect)
+        if (tag == GetTag.NotRefrect)
             return false;
 
         return true;
