40e8557c65b62b644afcfc394cc31529975cdd72
takumi okazaki <takumi9912@gmail.com>
2019-03-22 18:37:24 +0900

    自機のモデルを変更、落下死実装、他微修正

--- TokyoGame/Assets/Resources/Script/Okazaki/CameraMove.cs
+++ TokyoGame/Assets/Resources/Script/Okazaki/CameraMove.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    const float DEATH_HEIGHT = -5f;
     GameObject Player;
+    PlayerMove playerMove;
     Vector3 position = Vector3.zero;
 
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
+        playerMove = Player.GetComponent<PlayerMove>();
     }
-    
+
     void Update()
     {
-        if(Player.transform.position.y > DEATH_HEIGHT)
+        if(Player.transform.position.y > playerMove.deathHeight + 5f)
         {
             position.x = Player.transform.position.x;
         }
 
-        if (Player.transform.position.y <= DEATH_HEIGHT + 5f && Player.transform.position.y > DEATH_HEIGHT)
+        if (Player.transform.position.y <= playerMove.deathHeight + 10f && Player.transform.position.y > playerMove.deathHeight + 5f)
         {
-            position.y = DEATH_HEIGHT + 7f;
+            position.y = playerMove.deathHeight + 12f;
         }
-        else if(Player.transform.position.y > DEATH_HEIGHT + 5f)
+        else if(Player.transform.position.y > playerMove.deathHeight + 5f)
         {
             position.y = Player.transform.position.y + 2f;
         }
